8ae61daf113f7f1b8ac3f202b7c362dd210edd70
aldermann <trancongvietan22@gmail.com>
2020-05-21 21:43:17 +0700

    GameManager  delete GameObject when released

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -22,22 +22,13 @@ int main() {
     auto game = GameManager::createInstance(false);
     Renderer::createInstance("My game", SCREEN_W, SCREEN_H);
     Physics::World::init(200);
+    // Todo: Move this into a new event
     GameObject* ball = new Player({100, 300}, 200);
-    // GameObject* floor = new Floor({600, 700}, 1200, 40, 0);
-    // GameObject* wall1 = new Wall({1180, 350}, 40, 700, 0);
-    // GameObject* wall2 = new Wall({20, 350}, 40, 700, 0);
     GameManager::registerObject(ball);
-    // GameManager::registerObject(floor);
-    // GameManager::registerObject(wall1);
-    // GameManager::registerObject(wall2);
+    // endTodo
     game->timeline.schedule(new MapCreator(0));
     game->timeline.schedule(new SpawnEnemyEvent(1));
     GameManager::loop();
-    delete ball;
-    // delete floor;
-
-    //for (int i = 0; i < (int)floors.size(); ++i) delete floors[i];
-
     GameManager::releaseInstance();
     SDL_Quit();
   } catch (const AppError& err) {
